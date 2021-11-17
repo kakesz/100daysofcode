0c301e5511b6713d9abeba40ef3ef032fcc364d8
kakes <69126172+kakesz@users.noreply.github.com>
2021-11-17 16:55:27 -0300

    Update and rename mae gorda.py to altura.py

---
+++ altura.py
@@ -0,0 +1,5 @@
+tamanhop = int(input("Digite aqui seu tamanho em metros"))
+if tamanhop >= 1.7:
+    print("Sua altura é acima da média no Brasil!")
+else:
+    print ("Sua altura é "tamanhop" metros")
--- mae gorda.py
+++
@@ -1,5 +0,0 @@
-maegorda = int(input("Digite aqui o tamanho da sua mae em metros"))
-if maegorda >= 10:
-    print("sua mae e bem gorda ein slc")
-else:
-    print ("sua mae e gorda mas nem tanto tmj")
